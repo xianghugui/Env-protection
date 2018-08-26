d25abb4cb77fab31bf46c9ddb09dbbbea380a38a
xianghugui <arnoldlve@icloud.com>
2018-08-26 18:10:56 +0800

    update

--- src/main/java/com/environment/program/ProgramApplication.java
+++ src/main/java/com/environment/program/ProgramApplication.java
@@ -3,11 +3,28 @@ package com.environment.program;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
 @MapperScan("com.environment.program.dao")
 @SpringBootApplication
 public class ProgramApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProgramApplication.class, args);
+
+        try {
+            Connect.connect();
+             Thread tRecv = new Thread(new Connect.RecvThread());
+            tRecv.start();
+        } catch (UnknownHostException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
+
 }
