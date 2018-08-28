cdd2623919a1f1ac8fd8462af96668af40343d92
ChenXiaohui <15606951991@qq.com>
2018-08-28 17:56:53 +0800

    改为service注入

--- src/main/java/com/environment/program/ProgramApplication.java
+++ src/main/java/com/environment/program/ProgramApplication.java
@@ -3,12 +3,6 @@ package com.environment.program;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.io.IOException;
-import java.net.UnknownHostException;
 
 @MapperScan("com.environment.program.dao")
 @SpringBootApplication
